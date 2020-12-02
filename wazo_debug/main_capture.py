9917ed5a55b70cae9acdc890e47cf5781bd17b83
Sebastien Duthil <sduthil@wazo.community>
2020-12-02 16:56:48 -0500

    wazo-debug-capture: refactor subprocess

--- wazo_debug/main_capture.py
+++ wazo_debug/main_capture.py
@@ -6,20 +6,17 @@ from subprocess import call, Popen
 
 class CaptureCommand:
     def __init__(self):
-        self.asterisk_log_process = None
+        self.log_processes = []
 
     def take_action(self):
         call(['mkdir', '-p', '/tmp/wazo-debug-capture'])
-        self.asterisk_log_process = Popen('tail -f /var/log/asterisk/full > /tmp/wazo-debug-capture/asterisk-full', shell=True)
-        self.wazo_auth_log_process = Popen('tail -f /var/log/wazo-auth.log > /tmp/wazo-debug-capture/wazo-auth.log', shell=True)
+        self.log_processes.append(Popen('tail -f /var/log/asterisk/full > /tmp/wazo-debug-capture/asterisk-full', shell=True))
+        self.log_processes.append(Popen('tail -f /var/log/wazo-auth.log > /tmp/wazo-debug-capture/wazo-auth.log', shell=True))
 
     def clean_up(self):
-        if self.asterisk_log_process:
-            self.asterisk_log_process.kill()
-            self.asterisk_log_process.wait()
-        if self.wazo_auth_log_process:
-            self.wazo_auth_log_process.kill()
-            self.wazo_auth_log_process.wait()
+        for process in self.log_processes:
+            process.kill()
+            process.wait()
 
 
 def main():
