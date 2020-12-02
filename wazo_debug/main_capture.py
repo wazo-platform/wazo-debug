14fd6d8b937dd54694b917a54ed72bf4bb1ef8ef
Sebastien Duthil <sduthil@wazo.community>
2020-12-02 16:54:03 -0500

    wazo-debug-capture: add wazo-auth logs

--- wazo_debug/main_capture.py
+++ wazo_debug/main_capture.py
@@ -11,11 +11,15 @@ class CaptureCommand:
     def take_action(self):
         call(['mkdir', '-p', '/tmp/wazo-debug-capture'])
         self.asterisk_log_process = Popen('tail -f /var/log/asterisk/full > /tmp/wazo-debug-capture/asterisk-full', shell=True)
+        self.wazo_auth_log_process = Popen('tail -f /var/log/wazo-auth.log > /tmp/wazo-debug-capture/wazo-auth.log', shell=True)
 
     def clean_up(self):
         if self.asterisk_log_process:
             self.asterisk_log_process.kill()
             self.asterisk_log_process.wait()
+        if self.wazo_auth_log_process:
+            self.wazo_auth_log_process.kill()
+            self.wazo_auth_log_process.wait()
 
 
 def main():
