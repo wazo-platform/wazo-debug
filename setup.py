2558c118e48e73c3bb092f84b5465267cf309866
Francois Blackburn <fblackburn@wazo.community>
2020-12-22 14:08:20 -0500

    fix copyright date

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from setuptools import setup
