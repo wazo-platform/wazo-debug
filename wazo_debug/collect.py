0d27d0616664887fe9e917e0203bf0f99ce0ae93
Sebastien Duthil <sduthil@wazo.community>
2022-08-02 15:22:22 -0400

    collect: make timedatectl output machine-readable

--- wazo_debug/collect.py
+++ wazo_debug/collect.py
@@ -90,7 +90,7 @@ def gather_engine_info(gathering_directory):
     command = ['rsync', '-a', '/usr/share/wazo/WAZO-VERSION', gathering_directory]
     call(command)
 
-    command = ['timedatectl']
+    command = ['timedatectl', 'show']
     with open(f'{gathering_directory}/timedatectl.txt', 'a') as info_file:
         call(command, stdout=info_file)
 
